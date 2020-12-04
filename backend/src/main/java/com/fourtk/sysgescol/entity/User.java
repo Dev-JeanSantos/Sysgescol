eb0123e8294c280f216caba250d5d2fdd9fff90d
Jean Santos <jeancbsan@gmail.com>
2020-12-04 18:13:57 -0300

    User and Role entities created, JPA and Seed mapping implemented

---
+++ backend/src/main/java/com/fourtk/sysgescol/entity/User.java
@@ -0,0 +1,185 @@
+package com.fourtk.sysgescol.entity;
+
+import java.io.Serializable;
+import java.time.Instant;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_user")
+public class User implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String name;
+	private String registration;
+	@Column(unique = true)
+	private String email;
+	private String cpf;
+	private String rg;
+	private String genre;
+	private Instant dateOfBirth;
+	private String address;
+	private String contact;
+	private String imgUrl;
+	private String password;
+	
+	public User() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+
+	public User(Long id, String name, String registration, String email, String cpf, String rg, String genre,
+			Instant dateOfBirth, String address, String contact, String imgUrl, String password) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.registration = registration;
+		this.email = email;
+		this.cpf = cpf;
+		this.rg = rg;
+		this.genre = genre;
+		this.dateOfBirth = dateOfBirth;
+		this.address = address;
+		this.contact = contact;
+		this.imgUrl = imgUrl;
+		this.password = password;
+	}
+
+
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getRegistration() {
+		return registration;
+	}
+
+	public void setRegistration(String registration) {
+		this.registration = registration;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getRg() {
+		return rg;
+	}
+
+	public void setRg(String rg) {
+		this.rg = rg;
+	}
+
+	public String getGenre() {
+		return genre;
+	}
+
+	public void setGenre(String genre) {
+		this.genre = genre;
+	}
+
+	public Instant getDateOfBirth() {
+		return dateOfBirth;
+	}
+
+	public void setDateOfBirth(Instant dateOfBirth) {
+		this.dateOfBirth = dateOfBirth;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getContact() {
+		return contact;
+	}
+
+	public void setContact(String contact) {
+		this.contact = contact;
+	}
+
+	public String getImgUrl() {
+		return imgUrl;
+	}
+
+	public void setImgUrl(String imgUrl) {
+		this.imgUrl = imgUrl;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	
+}
---
+++ backend/src/main/resources/import.sql
@@ -0,0 +1,9 @@
+INSERT INTO tb_user (name, registration, email, cpf, rg, genre, date_Of_Birth, address, contact, img_Url, password) VALUES ('JEAN CHARLES BATISTA SANTOS', '101', 'jeancbsan@gmail.com', '676253985-87','35910662-6', 'Masculino',TIMESTAMP WITH TIME ZONE '1977-08-09T20:50:07.12345Z','Rua Manoel Novis 201 Centro Tanguá-RJ CEP:24890-000','021-97993-2737','imagemjean.jpg','$2a$10$eACCYoNOHEqXve8aIWT8Nu3PkMXWBaOxJ9aORUYzfMQCbVBIhZ8tG');
+INSERT INTO tb_user (name, registration, email, cpf, rg, genre, date_Of_Birth, address, contact, img_Url, password) VALUES ('CAMILLA DIAS DUARTE SANTOS', '102', 'camilladdsan@gmail.com', '676253985-87','35910662-6', 'Feminino',TIMESTAMP WITH TIME ZONE '1979-04-18T20:50:07.12345Z','Rua Manoel Novis 201 Centro Tanguá-RJ CEP:24890-000','021-97993-2737','imagemcamilla.jpg','$2a$10$eACCYoNOHEqXve8aIWT8Nu3PkMXWBaOxJ9aORUYzfMQCbVBIhZ8tG');
+INSERT INTO tb_user (name, registration, email, cpf, rg, genre, date_Of_Birth, address, contact, img_Url, password) VALUES ('JEAN CHARLES DUARTE SANTOS', '103', 'jeancduasan@gmail.com', '676253985-87','35910662-6', 'Masculino',TIMESTAMP WITH TIME ZONE '2004-12-18T20:50:07.12345Z','Rua Manoel Novis 201 Centro Tanguá-RJ CEP:24890-000','021-97993-2737','imagemjeanjr.jpg','$2a$10$eACCYoNOHEqXve8aIWT8Nu3PkMXWBaOxJ9aORUYzfMQCbVBIhZ8tG');
+INSERT INTO tb_user (name, registration, email, cpf, rg, genre, date_Of_Birth, address, contact, img_Url, password) VALUES ('MELISSA DUARTE SANTOS', '104', 'melgameruser@gmail.com', '676253985-87','35910662-6', 'Masculino',TIMESTAMP WITH TIME ZONE '2004-12-18T20:50:07.12345Z','Rua Manoel Novis 201 Centro Tanguá-RJ CEP:24890-000','021-97993-2737','imagemjeanjr.jpg','$2a$10$eACCYoNOHEqXve8aIWT8Nu3PkMXWBaOxJ9aORUYzfMQCbVBIhZ8tG');
+
+
+
+INSERT INTO tb_role (authority) VALUES ('ROLE_OPERATOR');
+INSERT INTO tb_role (authority) VALUES ('ROLE_ADMIN');

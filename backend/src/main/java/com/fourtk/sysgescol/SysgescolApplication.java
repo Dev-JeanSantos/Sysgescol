07470bdfc3170294eb35dccbee99deebfb4302ea
Jean Santos <jeancbsan@gmail.com>
2020-12-04 16:55:36 -0300

    Created project

---
+++ backend/src/main/java/com/fourtk/sysgescol/SysgescolApplication.java
@@ -0,0 +1,13 @@
+package com.fourtk.sysgescol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SysgescolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SysgescolApplication.class, args);
+	}
+
+}
